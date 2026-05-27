1d885f15b9d721fc277fdaa14bb1fd4f7034da0b
V2KX0 <ssamneang249@gmail.com>
2026-05-28 01:03:06 +0700

    Configure Vite and Vue Router for GitHub Pages

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './assets/pages/HomeView.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Home', component: HomeView },
     { path: '/shop', name: 'Shop', component: () => import('./assets/pages/ShopView.vue') },
